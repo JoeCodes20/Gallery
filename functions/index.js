9e84f52fde91c141ed8a74b0e96fa986122a7698
Joseph Adeyanju <transparentvfx@gmail.com>
2021-05-26 13:18:47 -0400

    "update"

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,9 @@
 const functions = require("firebase-functions");
-const express = require("express");
-const app = express()
 
-
-app.listen(3001,()=>{
-       console.log()
-})
-app.get("/", (req, res)=>{
-       res.send("hello world created throw node.js")
-})
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest(app);
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
